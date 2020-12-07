fabe488ce8dcea57bfaa5fd442f455b05e6a2ef2
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2020-12-07 17:42:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import jsonServer from './apis/jsonServer';
 
 const App = async () => {
     const res = await jsonServer.get('/0');
-    // console.log(res.data)
+    console.log(res.data)
 
     return <h1>Hello world</h1>;
 }
